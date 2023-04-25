71ed058f9822a02c1898eaae237e99e1d12e0875
deforeloop <infinitestrife@gmail.com>
2023-04-25 16:50:10 +0000

    join and split

---
+++ lab_input/challenge18.py
@@ -0,0 +1,9 @@
+### Challenge 18 ###
+
+def main():
+
+    name = input("Yo dawg, what's your name? ")
+    weekday = input("And what day of the week is it today? ")
+    print("Hello,", name + "! Happy", weekday + "!")
+
+main()
